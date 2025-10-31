32cc63892d4fc5d236b5c649c40c027946f3507d
kunalh17 <kunalh_bhatt@yahoo.co.in>
2025-10-31 17:56:17 +0530

    Initial Terraform EC2 setup

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  required_version = ">= 1.5.0"
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-0dee22c13ea7a9a67" # Amazon Linux 2 (ap-south-1)
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Terraform-GitHub-EC2"
+  }
+}
+
+output "instance_id" {
+  value = aws_instance.my_ec2.id
+}
