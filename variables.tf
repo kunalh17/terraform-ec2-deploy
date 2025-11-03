b001851dd8564c779b1b24289f1ad7cef3d18c53
kunalh17 <kunalh_bhatt@yahoo.co.in>
2025-11-03 20:35:34 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,17 @@
-variable "aws_region" {
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "region" {
   description = "AWS region"
   type        = string
   default     = "ap-south-1"
+}
+
+variable "key_name" {
+  description = "EC2 key pair name"
+  type        = string
+  default     = null
 }
